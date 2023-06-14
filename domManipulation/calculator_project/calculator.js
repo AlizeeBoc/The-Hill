2dc071d83abaa6f9f63ce606ffad93235331d14d
Alizee <bocianalizee@gmail.com>
2023-06-14 17:41:51 +0200

    starting calculator_project : html; début du raisonnement sur js

---
+++ domManipulation/calculator_project/calculator.js
@@ -0,0 +1,21 @@
+//Calculatrice : 
+// haut : écran
+// bas : tabs = boutons
+//     --> eventlistener sur chacune click + sa fonction
+
+
+// TODO :
+//     --> array tabs =[x*{name, whatisdisplay, function},]
+//
+
+
+
+// responsive : sur grand écran , calculatrice scientifique (div supplémentaire en flex-row)
+
+
+
+document.querySelector("main")
+let myCalculator = document.createElement("div")
+let top_screen = document.createElement("div")
+let bottom = document.createElement("div")
+let key = document.createElement("div")        // touches : div ou para?
