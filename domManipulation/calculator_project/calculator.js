0b6d147e5e36f595efe4b63fd9f8366c53d34a97
Alizee <bocianalizee@gmail.com>
2023-06-17 06:49:39 +0200

    deuxième calculatrice : 1ere shitty start

--- domManipulation/calculator_project/calculator.js
+++ domManipulation/calculator_project/calculator.js
@@ -1,75 +1,153 @@
-//////////////////////////////////BRAINSTORM//////////////////////////////////
-//Container calculatrice : 
-// div top : écran
-// div bottom : keys = buttons
-//                     --> eventlistener sur chacune (click + sa fonction propre)   --> e.preventdefault() pour empecher que le formulair soit submit et que la page se recharge a chaque click
-// (facultatif : sur gr écran bottom : 2 div (normal + scientifique))
+///////////:problemes a regler
+// comment clear l'écran apres une opération de facon auto? innerText = "" puis innerText = input
+//comment afficher dans le operationScreen l'input avant le =
+// % comment pourcentage pas modulo?
 
-//////////////////////////////////TODO before I start //////////////////////////////////
-//se renseigner sur les attributs des buttons
+let body = document.body;
+body.style.margin = "0";
+body.style.padding = "0";
+body.style.boxSizing = "border-box";
+let container = document.querySelector("main");
+let myCalculator = document.createElement("div");
+let top_Screen = document.createElement("div");
+let operationScreen = document.createElement("div");
+let outputScreen = document.createElement("div");
+let bottom = document.createElement("div");
 
-//////////////////////////////////WHATODOOO//////////////////////////////////
-//1. HTML + CSS de base
-//2. AddEventListener sur tous les boutons
-//   ---> si bouton est chiffre : valeur du chiffre
-//   ---> si bouton est opérateur : 
-
-
-
-let container = document.querySelector("main")
-let myCalculator = document.createElement("div")
-let topScreen = document.createElement("div")
-let operationScreen = document.createElement("div")
-let outputScreen = document.createElement("div")
-let bottom = document.createElement("div")
-
-outputScreen.innerText = 0
-topScreen.appendChild(outputScreen)
-topScreen.appendChild(operationScreen)
+outputScreen.innerText = "";
+top_Screen.appendChild(operationScreen);
+top_Screen.appendChild(outputScreen);
 /* bottom.appendChild(key) */
-myCalculator.appendChild(topScreen)
-myCalculator.appendChild(bottom)
-container.appendChild(myCalculator)
-
+top_Screen.style.border = "1px solid black";
+top_Screen.style.borderRadius = "3px";
+top_Screen.style.padding = "15px";
+top_Screen.style.textAlign = "right";
+myCalculator.appendChild(top_Screen);
+myCalculator.appendChild(bottom);
+myCalculator.style.width = "350px";
+myCalculator.style.margin = "auto";
+myCalculator.style.marginTop = "200px";
+container.appendChild(myCalculator);
 
 //création des boutons
-var i = 0
-const myKeys = ["C", "%", "/", 7, 8, 9, "X", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "="]
+
+const myKeys = [
+  "C",
+  "%",
+  "/",
+  7,
+  8,
+  9,
+  "*",
+  4,
+  5,
+  6,
+  "-",
+  1,
+  2,
+  3,
+  "+",
+  0,
+  ".",
+  "=",
+];
+var i = 0;
 
 while (i < 18) {
   var button = document.createElement("button");
-
-  var buttonContent = myKeys[i]
-  
-  button.textContent = buttonContent ;
+  button.textContent = myKeys[i];
+  bottom.style.display = "grid";
+  bottom.style.gridTemplateColumns = "1fr 1fr 1fr 1fr"; // src qui déchire = "https://css-tricks.com/snippets/css/complete-guide-grid/"
+  bottom.style.gridTemplateRows = "1fr 1fr 1fr 1fr 1fr";
+  bottom.style.rowGap = "1px";
+  bottom.style.columnGap = "1px";
+  button.style.padding = "15px";
+  button.style.borderRadius = "3px";
   bottom.appendChild(button);
-
   i++;
 }
 
-// Attribuer classe aux boutons chiffres
-const arrayOfNumbers = [3, 4, 5, 7, 8, 9, 11, 12, 13, 15]  //créer une array avec les index des boutons cibles
+// classList.add ... chiffres
+const arrayOfNumbers = [3, 4, 5, 7, 8, 9, 11, 12, 13, 15]; //créer une array avec les index des boutons cibles
 
 for (let i = 0; i < arrayOfNumbers.length; i++) {
-  /* const index = arrayOfNumbers[i] */
-  const numButton = document.getElementsByTagName("button")[arrayOfNumbers[i]];  //on séléctionne les boutons qui correspondent a ces index
-  numButton.classList.add("numKey")                                              // classList.add()
+  const numButton = document.getElementsByTagName("button")[arrayOfNumbers[i]]; //on séléctionne dans myKeys les boutons qui correspondent a ces index
+  numButton.classList.add("numKey"); // classList.add()
 }
 
+// classList.add ... opérateurs
+const arrayOfOperators = [2, 6, 10, 14];
+for (let i = 0; i < arrayOfOperators.length; i++) {
+  //////////// pas necessaire avec la fct de Diogo////////////////
+  let index = arrayOfOperators[i];
+  let operatorButton = document.getElementsByTagName("button")[index];
+  operatorButton.classList.add("opertorKey");
+}
 
+// classList.add ... aux symboles "C" et "="
+const clearButton = document.getElementsByTagName("button")[0];
+clearButton.classList.add("clearKey");
+clearButton.style.gridColumn = "span 2";
 
+const equalbutton = document.getElementsByTagName("button")[17];
+equalbutton.classList.add("equalKey");
+equalbutton.style.gridColumn = "span 2";
 
+// addEventListener sur tous boutons (si c -> clear, si = -> calcule, si chiffre -> affiche chiffre)
 
-////////////////// Procédé ///////////////////////////
+function computeResult(input) {
+  return Function("return " + input)(); ///!!! "return" !== "return " : owiOwi!!!!
+}
+
+let myButtons = bottom.querySelectorAll("button");
+
+let resultDisplayed = false; //drapeau baissé
+
+myButtons.forEach((button) => {
+  //EventListener sur les boutons. condition pour "C" et "="
+  button.style.backgroundColor = "black";
+  button.style.color = "white";
+
+  button.addEventListener("click", (e) => {
+    if (e.target.innerText === "C") {
+      outputScreen.innerText = "";
+      operationScreen.innerText = "";
+    } else if (e.target.innerText == "=") {
+      operationScreen.append(outputScreen.innerText);
+      outputScreen.innerText = computeResult(displayed);
+      resultDisplayed = true; //drapeau levé
+    } else {
+      if (resultDisplayed === false) {
+        // dans tous les autres cas tant qu'on n'a pas clické sur = avant :
+        displayed = outputScreen.innerText += e.target.innerText;
+        console.log(displayed);
+      } else {
+        // si on a cliqué sur "=" avant
+        displayed = outputScreen.innerText = e.target.innerText;
+        operationScreen.innerText = "";
+        console.log(displayed);
+        /*         if ((e.target.classList = operatorButton)) {
+          displayed = outputScreen.innerText = computeResult(displayed) += operatorButton.innerText;
+        } else {
+          displayed = outputScreen.innerText = e.target.innerText;
+          operationScreen.innerText = "";} */
+        resultDisplayed = false;
+      }
+    }
+  });
+});
+
 /* 
-1. createElement + appendChil() + while loop pour créer les boutons
-2. 
+let restart = (outputScreen) => {
+  outputScreen.innerText = "";
+};                                                                              ///////////////YOU ARE LOST SOMEWHERE HERE///////////////////////
+
+if ((outputScreen.innerText = computeResult(input))) {
+  restart;
+}
  */
 
-
-
-
-
+// ------------------------------------
 
 ///////////////// DONNE PAR DIOGO /////////////////
 
@@ -81,13 +159,11 @@ for (let i = 0; i < arrayOfNumbers.length; i++) {
   
   console.log(computeResult(a)) // Should display 10 */
 
-
-
-///////////////// FACULTATIF /////////////////
+///////////////// TODO : (FACULTATIF) /////////////////
 
 /* 
 Make it pretty : naaaaah!
-Create an history of the previous operations  : div supplémentaire a droite avec console log (a déterminer)
+Create an history of the previous operations  : div supplémentaire a droite (a déterminer)
 Scientific calculator (trigonometry, logarithms, ...). The Math library is pretty convenient for this (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math) : div supplémentaire gauche ssi affichage sur gr écran
 Unit conversion (could be money or imperial units to metric system for example) : ajouter un boutton "curseur" --> eventlistener fct est une opération de conversion qui opere sur tout le code?
 Pressing enter to display the result (same as pressing the = button) : eventListener keydown Enter, fonction = calculate?
---
+++ domManipulation/calculator_projectBis/calculatorBis.js
@@ -0,0 +1,141 @@
+///////////:problemes a regler
+// % comment pourcentage pas modulo?
+// comment clear l'écran apres une opération de facon auto? innerText = "" puis innerText = input
+//comment afficher dans le operationScreen l'input avant le =
+
+let body = document.body;
+body.style.margin = "0";
+body.style.padding = "0";
+body.style.boxSizing = "border-box";
+let container = document.querySelector("main");
+let myCalculator = document.createElement("div");
+let top_Screen = document.createElement("div");
+let operationScreen = document.createElement("div");
+let outputScreen = document.createElement("div");
+let bottom = document.createElement("div");
+
+outputScreen.innerText = "";
+outputScreen.style.fontSize = "35px";
+operationScreen.innerText = "";
+operationScreen.style.fontSize = "25px";
+top_Screen.appendChild(operationScreen);
+top_Screen.appendChild(outputScreen);
+/* bottom.appendChild(key) */
+top_Screen.style.border = "2px solid #d3d3d3";
+top_Screen.style.borderRadius = "10px";
+top_Screen.style.padding = "15px";
+top_Screen.style.margin = "10px 0px";
+top_Screen.style.textAlign = "right";
+top_Screen.style.height = "65px";
+myCalculator.appendChild(top_Screen);
+myCalculator.appendChild(bottom);
+myCalculator.style.width = "630px";
+myCalculator.style.height = "fit-content";
+myCalculator.style.margin = "auto";
+myCalculator.style.marginTop = "120px";
+myCalculator.style.border = "2px solid #d3d3d3";
+myCalculator.style.padding = "20px";
+myCalculator.style.borderRadius = "10px";
+container.appendChild(myCalculator);
+
+//création des boutons
+
+const myKeys = [
+  "C",
+  "%",
+  "/",
+  7,
+  8,
+  9,
+  "*",
+  4,
+  5,
+  6,
+  "-",
+  1,
+  2,
+  3,
+  "+",
+  0,
+  ".",
+  "=",
+];
+
+var i = 0;
+
+while (i < 18) {
+  var button = document.createElement("button");
+  button.textContent = myKeys[i];
+  bottom.style.display = "grid";
+  bottom.style.gridTemplateColumns = "1fr 1fr 1fr 1fr"; // src qui déchire = "https://css-tricks.com/snippets/css/complete-guide-grid/"
+  bottom.style.gridTemplateRows = "1fr 1fr 1fr 1fr 1fr";
+  bottom.style.rowGap = "10px";
+  bottom.style.columnGap = "10px";
+  button.style.padding = "15px";
+  button.style.borderRadius = "10px";
+  button.style.fontSize = "25px";
+  button.style.border = "none";
+  button.style.height = "auto";
+  bottom.appendChild(button);
+  i++;
+}
+
+// grid span 2 pour C et =
+let clearButton = document.getElementsByTagName("button")[0];
+clearButton.style.gridColumn = "span 2";
+
+let equalbutton = document.getElementsByTagName("button")[17];
+equalbutton.style.gridColumn = "span 2";
+
+//style.buttons
+const greyButtonsIndex = [0, 1, 2, 6, 10, 14];
+for (let i = 0; i < greyButtonsIndex.length; i++) {
+  const greyButtons =
+    document.getElementsByTagName("button")[greyButtonsIndex[i]];
+  greyButtons.style.backgroundColor = "#a8a8a7";
+}
+
+const equalButton = document.getElementsByTagName("button")[17];
+equalButton.style.backgroundColor = "#1c8ced";
+
+///////////////////////////// GO ///////////////////////////////////////////
+
+//3. Array vide dans laquelle on va push la valeur de ce sur quoi on click...
+let myInputs = [];
+let accumulativeInputs;
+function computeResult(input) {
+  return Function("return " + input)(); ///!!! return espace avant les guillemets!!!!
+}
+/* function percentages(input) {
+  additionne les valeurs qui précèdent le % et les /100
+} */
+
+//2.callback
+//2.1. push les inputs dans un array vide --> console.log (myInputs) pour visualiser l'array
+//2.2. déclare myInput.join("") pour ensuite les afficher sans la ","
+//2.3. if, else if, else
+
+let calculate = (button) => {
+  const input = button.textContent;
+  if (input === "C") {
+    outputScreen.innerText = "";
+    operationScreen.innerText = "";
+    myInputs = [""];
+  } else if (input === "=") {
+    operationScreen.append(outputScreen.innerText);
+    outputScreen.innerText = computeResult(accumulativeInputs);
+  } /* else if (input === "%") {
+    outputScreen.innerText = percentages(accumulativeInputs);
+  }  */ else {
+    myInputs.push(input); //console.log(myInputs) pour visualiser l'array
+    accumulativeInputs = myInputs.join(""); //console.log(accumulativeInputs) pour visualiser les opérations
+    outputScreen.innerText = accumulativeInputs;
+  }
+};
+
+//1. eventListener sur chaque bouton --> callback (step2)
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  //forEach ne fonctionnerait que sur des élmts html????
+  button.addEventListener("click", () => calculate(button)); //soit {}, soit fct directement
+});
