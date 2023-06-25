8af4169b66cce3ebff709c6828f2f6ffe63d2893
Alizee <bocianalizee@gmail.com>
2023-06-25 11:47:17 +0200

    exercice cloturé + extra1 + Css. Reste à résoudre le problème des divs générées à chaque fetch

--- asynch_fetch/agifyAPI/agify.js
+++ asynch_fetch/agifyAPI/agify.js
@@ -6,7 +6,7 @@ Keep the the past requests on the page by creating a new div each time you make 
 
 // ----> POST REQUEST
 
-let body = document.body;
+/* let body = document.body;
 const submit = document.getElementById("submit");
 submit.addEventListener("click", fetchDatas);
 
@@ -25,5 +25,55 @@ function fetchDatas() {
       body.insertBefore(myDiv, body.children[1]);
     })
     .catch((error) => console.log(error));            //Et si ca ne marche pas ...
+} */
+
+const body = document.body;
+const submit = document.getElementById("submit");
+// const resultDiv = document.getElementById("result");
+
+submit.addEventListener("click", fetchDatas);
+
+function generateCountryOptions(jsonData) {
+  const selectCountry = document.getElementById("country");
+
+  for (const code in jsonData) {
+    const countryName = jsonData[code];
+    const option = document.createElement("option");
+    option.value = code;
+    option.textContent = countryName;
+    selectCountry.appendChild(option);
+  }
 }
 
+function fetchDatas() {
+  const nameInput = document.getElementById("name");
+  const name = nameInput.value;
+
+  const selectCountry = document.getElementById("country");
+  const countryCode = selectCountry.value;
+  const countryName = selectCountry.options[selectCountry.selectedIndex].text;
+
+  const myDiv = document.createElement("div");
+  myDiv.id = "messageDiplayed";
+  body.insertBefore(myDiv, body.children[2]);
+
+  fetch(`https://api.agify.io?name=${name}&country_id=${countryCode}`) //!!countryCode, pas countryName!!!
+    .then((response) => response.json())
+    .then((data) => {
+      const age = data.age;
+      myDiv.textContent = `The name ${name} is approximately ${age} years old in ${countryName}.\n`;
+    })
+    .catch((error) => {
+      console.log(error);
+      myDiv.textContent = "An error occurred while fetching data.";
+    });
+}
+
+// Charger le fichier JSON contenant les informations sur les pays
+fetch("countries.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Appeler la fonction pour générer les options du menu déroulant
+    generateCountryOptions(data);
+  })
+  .catch((error) => console.log(error));
